c041d1da3910269ac5ec1b0deca27617aba5e411
Kyle Maass <kmaass@gmail.com>
2013-07-18 22:39:11 -0400

    Added JKTechnologies.Practice.Katas project file for functions.

---
+++ JKTechnologies.Practice.Katas/JKTechnologies.Practice.Katas.FunctionsApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JKTechnologies.Practice.Katas.FunctionsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WTF Mate");
+
+        }
+    }
+}
